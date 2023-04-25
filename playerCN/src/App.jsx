a7fba29c81bf96744060c1ebc9229513e29bd528
Rodri7Roman <romanrodri03@gmail.com>
2023-04-25 18:01:33 -0300

    init

---
+++ playerCN/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./components/Home/Home";
+import { Provider } from "react-redux";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import store from "./redux/store";
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
