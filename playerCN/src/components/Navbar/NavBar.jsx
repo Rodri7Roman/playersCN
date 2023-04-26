e7b9d4b3c493de00b698f4af7133963f05066388
Rodri7Roman <romanrodri03@gmail.com>
2023-04-26 16:42:19 -0300

    navbar first

--- playerCN/src/components/Navbar/NavBar.jsx
+++ playerCN/src/components/Navbar/NavBar.jsx
@@ -4,42 +4,41 @@ import style from "./NavBar.module.css";
 import homeIcon from "../../assets/icons/home.svg";
 import messagesIcon from "../../assets/icons/messages.svg";
 import notis from "../../assets/icons/notis.svg";
-import "./style.css";
 
 const NavBar = (props) => {
   return (
     <nav className={`${style.navBar}  ${style.bg}`}>
       <div className={style.containerLogo}>
-        <a className={`navbar-brand ${style.textLinkNav}`} href="#">
+        <a className={`navbar-brand ${style.containerTextLinkLogo}`} href="#">
           <img src={logo} alt="logoGC" />
-          <p>GamersConnect</p>
+          <p className={style.textLogo}>GamersConnect</p>
         </a>
       </div>
 
       <div className={style.flexNav}>
         <ul className={`navbar-nav ${style.navBarNav}`}>
           <li className="nav-item">
-            <a href="" className={`nav-link ${style.textLinkNav}`}>
+            <a href="" className={`nav-link ${style.containerTextLinkNav}`}>
               <box-icon name="home-alt-2" color="#ffffff"></box-icon>
-              <p>Inicio</p>
+              <p className={style.textIcon}>Inicio</p>
             </a>
           </li>
           <li className="nav-item">
-            <a href="" className={`nav-link ${style.textLinkNav}`}>
+            <a href="" className={`nav-link ${style.containerTextLinkNav}`}>
               <box-icon name="message" color="#ffffff"></box-icon>
-              <p>Mensajes</p>
+              <p className={style.textIcon}>Mensajes</p>
             </a>
           </li>
           <li className="nav-item">
-            <a href="" className={`nav-link ${style.textLinkNav}`}>
+            <a href="" className={`nav-link ${style.containerTextLinkNav}`}>
               <box-icon name="bell" color="#ffffff"></box-icon>
-              <p>Notificaciones</p>
+              <p className={style.textIcon}>Notificaciones</p>
             </a>
           </li>
         </ul>
       </div>
       <div>
-        <p className={`nav-link ${style.textLinkNav}`}>OTRA COSA</p>
+        <p className={`nav-link ${style.containerTextLinkNav}`}>OTRA COSA</p>
       </div>
     </nav>
   );
