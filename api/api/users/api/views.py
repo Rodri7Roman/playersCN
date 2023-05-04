31aca4be9704450a9057858d63678a6cd004eab4
Rodri7Roman <romanrodri03@gmail.com>
2023-05-04 15:52:44 -0300

    login

--- api/api/users/api/views.py
+++ api/api/users/api/views.py
@@ -2,9 +2,16 @@ from users.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import UserRegisterSerializer
 
 
 class RegisterView(APIView):
     def post(self, request):
-        print("Registrando usuario")
-        return Response(status=status.HTTP_200_OK, data="Todo OK")
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+        
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
