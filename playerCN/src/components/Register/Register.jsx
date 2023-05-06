09e1893cfcf8648f22099910895aef9588636437
Rodri7Roman <romanrodri03@gmail.com>
2023-05-06 15:53:29 -0300

    form

--- playerCN/src/components/Register/Register.jsx
+++ playerCN/src/components/Register/Register.jsx
@@ -5,8 +5,25 @@ const Register = () => {
   return (
     <div className={style.containerRegister}>
       <div className={style.containerForm}>
-        <form action="">
-          <label htmlFor=""></label>
+        <form action="" className={style.form}>
+          <div>
+            <label htmlFor="" className={style.labelRegister}>
+              Correo Electronico:{" "}
+            </label>
+            <input type="text" className={style.inputRegister} />
+          </div>
+          <div>
+            <label htmlFor="" className={style.labelRegister}>
+              Username:{" "}
+            </label>
+            <input type="text" className={style.inputRegister} />
+          </div>
+          <div>
+            <label htmlFor="" className={style.labelRegister}>
+              Contraseña:{" "}
+            </label>
+            <input type="text" className={style.inputRegister} />
+          </div>
         </form>
       </div>
     </div>
