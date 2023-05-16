df54319efc4a4cc4216d6572be4cdbf20b4ff4f6
Rodri7Roman <romanrodri03@gmail.com>
2023-05-16 13:09:36 -0300

    register css

--- playerCN/src/components/Register/Register.jsx
+++ playerCN/src/components/Register/Register.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import style from "./Register.module.css";
+import logo from "../../assets/icons/logo_GC2.png";
+import "./register.css";
 
 const Register = () => {
   return (
     <div className={style.containerRegister}>
+      <div className={style.containerLogo}></div>
       <div className={style.containerForm}>
         <form action="" className={style.form}>
-          <div>
+          <div className={style.containerLogoForm}>
+            <img src={logo} alt="logoGC" />
+            <p className={style.textLogo}>GamersConnect</p>
+          </div>
+          <div className={style.containerInputLabel}>
             <label htmlFor="" className={style.labelRegister}>
-              Correo Electrónico:{" "}
+              Correo Electrónico{" "}
             </label>
             <input type="text" className={style.inputRegister} />
           </div>
-          <div>
+          <div className={style.containerInputLabel}>
             <label htmlFor="" className={style.labelRegister}>
-              Username:{" "}
+              Nombre de usuario{" "}
             </label>
             <input type="text" className={style.inputRegister} />
           </div>
-          <div>
+          <div className={style.containerInputLabel}>
             <label htmlFor="" className={style.labelRegister}>
-              Contraseña:{" "}
+              Contraseña{" "}
             </label>
             <input type="text" className={style.inputRegister} />
           </div>
