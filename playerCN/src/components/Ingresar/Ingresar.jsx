1e73b0085d4e440a353859eb33ffc9aafb73632a
Rodri7Roman <romanrodri03@gmail.com>
2023-05-26 15:40:38 -0300

    not ing reg

--- playerCN/src/components/Ingresar/Ingresar.jsx
+++ playerCN/src/components/Ingresar/Ingresar.jsx
@@ -3,17 +3,24 @@ import { useDispatch, useSelector } from "react-redux";
 import style from "./Ingresar.module.css";
 import logo from "../../assets/icons/logo.png";
 import robot from "../../assets/imgs/robot2.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { validate } from "./validator";
 import { ingresar } from "../../redux/actions/users/user";
 const Ingresar = (props) => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
   const [isSubmit, setIsSubmit] = useState(false);
   const [inputs, setInputs] = useState({
     username: "",
     password: "",
   });
   const [inputErrors, setInputErrors] = useState({});
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) navigate("/");
+  }, []);
 
   useEffect(() => {
     if (Object.keys(inputErrors).length === 0 && isSubmit) {
@@ -38,55 +45,64 @@ const Ingresar = (props) => {
     setIsSubmit(true);
   };
   return (
-    <div className={style.containerRegister}>
-      <span className={style.spanContainer}></span>
-      <div className={style.containerLogo}>
-        <img src={robot} alt="" />
-      </div>
-      <div className={style.containerForm}>
-        <form action="" className={style.form}>
-          <div className={style.containerLogoForm}>
-            <img src={logo} alt="logoGC" />
-            <p className={style.textLogo}>GamersConnect</p>
+    <>
+      {!token ? (
+        <div className={style.containerRegister}>
+          <span className={style.spanContainer}></span>
+          <div className={style.containerLogo}>
+            <img src={robot} alt="" />
           </div>
+          <div className={style.containerForm}>
+            <form action="" className={style.form}>
+              <div className={style.containerLogoForm}>
+                <img src={logo} alt="logoGC" />
+                <p className={style.textLogo}>GamersConnect</p>
+              </div>
 
-          <div className={style.containerInputLabel}>
-            <label htmlFor="" className={style.labelRegister}>
-              Nombre de usuario{" "}
-            </label>
-            <input
-              type="text"
-              name="username"
-              value={inputs.username}
-              className={style.inputRegister}
-              onChange={handleChange}
-            />
-            <span className={style.span}>{inputErrors.username}</span>
+              <div className={style.containerInputLabel}>
+                <label htmlFor="" className={style.labelRegister}>
+                  Nombre de usuario{" "}
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  value={inputs.username}
+                  className={style.inputRegister}
+                  onChange={handleChange}
+                />
+                <span className={style.span}>{inputErrors.username}</span>
+              </div>
+              <div className={style.containerInputLabel}>
+                <label htmlFor="" className={style.labelRegister}>
+                  Contraseña{" "}
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={inputs.password}
+                  className={style.inputRegister}
+                  onChange={handleChange}
+                />
+                <span className={style.span}>{inputErrors.password}</span>
+              </div>
+              <div className={style.containerButtons}>
+                <button
+                  onClick={submitIngresar}
+                  className={style.buttonIniciar}
+                >
+                  Iniciar Sesión
+                </button>
+                <NavLink to={"/registrarse"}>
+                  <button className={style.buttonRegister}>Registrarse </button>
+                </NavLink>
+              </div>
+            </form>
           </div>
-          <div className={style.containerInputLabel}>
-            <label htmlFor="" className={style.labelRegister}>
-              Contraseña{" "}
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={inputs.password}
-              className={style.inputRegister}
-              onChange={handleChange}
-            />
-            <span className={style.span}>{inputErrors.password}</span>
-          </div>
-          <div className={style.containerButtons}>
-            <button onClick={submitIngresar} className={style.buttonIniciar}>
-              Iniciar Sesión
-            </button>
-            <NavLink to={"/registrarse"}>
-              <button className={style.buttonRegister}>Registrarse </button>
-            </NavLink>
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
--- playerCN/src/components/Register/Register.jsx
+++ playerCN/src/components/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import style from "./Register.module.css";
 import logo from "../../assets/icons/logo.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { registerUser } from "../../redux/actions/users/user";
 import { useDispatch } from "react-redux";
 import { validate } from "./validator";
@@ -9,6 +9,8 @@ import robot from "../../assets/imgs/robot1.png";
 
 const Register = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
   const [isSubmit, setIsSubmit] = useState(false);
   const [inputs, setInputs] = useState({
     email: "",
@@ -16,6 +18,11 @@ const Register = () => {
     password: "",
   });
   const [inputErrors, setInputErrors] = useState({});
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) navigate("/");
+  }, []);
 
   useEffect(() => {
     if (Object.keys(inputErrors).length === 0 && isSubmit) {
@@ -41,67 +48,78 @@ const Register = () => {
   };
 
   return (
-    <div className={style.containerRegister}>
-      <span className={style.spanContainer}></span>
-      <div className={style.containerLogo}>
-        <img src={robot} alt="" />
-      </div>
-      <div className={style.containerForm}>
-        <form action="" className={style.form}>
-          <div className={style.containerLogoForm}>
-            <img src={logo} alt="logoGC" />
-            <p className={style.textLogo}>GamersConnect</p>
+    <>
+      {!token ? (
+        <div className={style.containerRegister}>
+          <span className={style.spanContainer}></span>
+          <div className={style.containerLogo}>
+            <img src={robot} alt="" />
           </div>
-          <div className={style.containerInputLabel}>
-            <label htmlFor="" className={style.labelRegister}>
-              Correo Electrónico{" "}
-            </label>
-            <input
-              type="text"
-              name="email"
-              value={inputs.email}
-              onChange={handleChange}
-              className={style.inputRegister}
-            />
-            <span className={style.span}>{inputErrors.email}</span>
+          <div className={style.containerForm}>
+            <form action="" className={style.form}>
+              <div className={style.containerLogoForm}>
+                <img src={logo} alt="logoGC" />
+                <p className={style.textLogo}>GamersConnect</p>
+              </div>
+              <div className={style.containerInputLabel}>
+                <label htmlFor="" className={style.labelRegister}>
+                  Correo Electrónico{" "}
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  value={inputs.email}
+                  onChange={handleChange}
+                  className={style.inputRegister}
+                />
+                <span className={style.span}>{inputErrors.email}</span>
+              </div>
+              <div className={style.containerInputLabel}>
+                <label htmlFor="" className={style.labelRegister}>
+                  Nombre de usuario{" "}
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  value={inputs.username}
+                  className={style.inputRegister}
+                  onChange={handleChange}
+                />
+                <span className={style.span}>{inputErrors.username}</span>
+              </div>
+              <div className={style.containerInputLabel}>
+                <label htmlFor="" className={style.labelRegister}>
+                  Contraseña{" "}
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={inputs.password}
+                  className={style.inputRegister}
+                  onChange={handleChange}
+                />
+                <span className={style.span}>{inputErrors.password}</span>
+              </div>
+              <div className={style.containerButtons}>
+                <button
+                  onClick={submitRegister}
+                  className={style.buttonRegister}
+                >
+                  Registrarse
+                </button>
+                <NavLink to={"/ingresar"}>
+                  <button className={style.buttonIniciar}>
+                    Iniciar Sesión
+                  </button>
+                </NavLink>
+              </div>
+            </form>
           </div>
-          <div className={style.containerInputLabel}>
-            <label htmlFor="" className={style.labelRegister}>
-              Nombre de usuario{" "}
-            </label>
-            <input
-              type="text"
-              name="username"
-              value={inputs.username}
-              className={style.inputRegister}
-              onChange={handleChange}
-            />
-            <span className={style.span}>{inputErrors.username}</span>
-          </div>
-          <div className={style.containerInputLabel}>
-            <label htmlFor="" className={style.labelRegister}>
-              Contraseña{" "}
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={inputs.password}
-              className={style.inputRegister}
-              onChange={handleChange}
-            />
-            <span className={style.span}>{inputErrors.password}</span>
-          </div>
-          <div className={style.containerButtons}>
-            <button onClick={submitRegister} className={style.buttonRegister}>
-              Registrarse
-            </button>
-            <NavLink to={"/ingresar"}>
-              <button className={style.buttonIniciar}>Iniciar Sesión</button>
-            </NavLink>
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
